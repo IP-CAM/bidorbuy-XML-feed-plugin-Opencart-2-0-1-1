2197db93b8a3e594ea93d875aca8f4839874927f
idksa <jay@mercurymusic.co.za>
2016-01-08 08:51:08 +0200

    Added main file
    Added main pugin file

---
+++ bobfeedoc2011.php
@@ -0,0 +1,80 @@
+<?php
+class ControllerFeedbobfeedoc2 extends Controller {
+	public function index() {
+			$output  = '<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>';
+
+			$this->load->model('catalog/category');
+
+			$this->load->model('catalog/product');
+
+			$this->load->model('tool/image');
+
+			$products = $this->model_catalog_product->getProducts();
+
+			$output .= '<ROOT>';
+			$output .= '<Products>';
+			
+			foreach ($products as $product) {
+				if ($product['description']) {
+					$output .= '<Product>';
+					$output .= '<ProductCode><![CDATA['.$product['model'].']]></ProductCode>';
+					$output .= '<Title><![CDATA['.$product['name'].']]></Title>';
+					$categories = $this->model_catalog_product->getCategories($product['product_id']);
+
+					foreach ($categories as $category) {
+						$path = $this->getPath($category['category_id']);
+
+						if ($path) {
+							$string = '';
+
+							foreach (explode('_', $path) as $path_id) {
+								$category_info = $this->model_catalog_category->getCategory($path_id);
+
+								if ($category_info) {
+									if (!$string) {
+										$string = $category_info['name'];
+									} else {
+										$string .= ' &gt; ' . $category_info['name'];
+									}
+								}
+							}
+
+							$output .= '<Category><![CDATA['.$string.']]></Category>';
+						}
+					}
+					
+					$output .= '<Price>' . $product['price'] . '</Price>';
+					$output .= '<ImageURL><![CDATA['.$product['image'].']]></ImageURL>';
+					$output .= '<Description><![CDATA['.utf8_substr(strip_tags(html_entity_decode($product['description'], ENT_QUOTES, 'UTF-8'))).']]></Description>';
+					$output .= '</Product>';
+				}
+			}
+
+			$output .= '</Products>';
+			$output .= '</ROOT>';
+
+			$this->response->addHeader('Content-Type: application/rss+xml');
+			$this->response->setOutput($output);
+		}
+	
+	protected function getPath($parent_id, $current_path = '') {
+		$category_info = $this->model_catalog_category->getCategory($parent_id);
+
+		if ($category_info) {
+			if (!$current_path) {
+				$new_path = $category_info['category_id'];
+			} else {
+				$new_path = $category_info['category_id'] . '_' . $current_path;
+			}
+
+			$path = $this->getPath($category_info['parent_id'], $new_path);
+
+			if ($path) {
+				return $path;
+			} else {
+				return $new_path;
+			}
+		}
+	}
+}
+?>
